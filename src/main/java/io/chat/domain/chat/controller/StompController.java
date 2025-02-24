44125f326f807691640f13b3d311d88d322a545d
Joo-Veloper <psjoo3515@gmail.com>
2025-02-24 21:43:32 +0900

    feat: STOMP-based WebSock chat function

---
+++ src/main/java/io/chat/domain/chat/controller/StompController.java
@@ -0,0 +1,18 @@
+package io.chat.domain.chat.controller;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StompController {
+
+    @MessageMapping("/{roomId}")
+    @SendTo("/topic/{roomId}")
+    public String sendMessage(@DestinationVariable Long roomId, String message) {
+        System.out.println(message);
+
+        return message;
+    }
+}
