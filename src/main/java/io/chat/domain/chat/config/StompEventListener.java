c158b61fad4ad8ce99bbfe8a04c442b692c3319b
Joo-Veloper <psjoo3515@gmail.com>
2025-02-27 13:27:54 +0900

    feat: Implement automatic session management with Spring and STOMP

---
+++ src/main/java/io/chat/domain/chat/config/StompEventListener.java
@@ -0,0 +1,35 @@
+package io.chat.domain.chat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+//스프링과 stomp는 내부적으로 세션관리를 자동으로 처리
+@Slf4j
+@Component
+public class StompEventListener {
+
+    private final Set<String> sessions = ConcurrentHashMap.newKeySet();
+
+    @EventListener
+    public void connectHandle(SessionConnectEvent event){
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        sessions.add(accessor.getSessionId());
+        log.info("connect session ID: {}", accessor.getSessionId());
+        log.info("total session: {}", sessions.size());
+    }
+
+    @EventListener
+    public void disconnectHandle(SessionDisconnectEvent event){
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        sessions.remove(accessor.getSessionId());
+        log.info("disconnect session ID: {}", accessor.getSessionId());
+        log.info("total session: {}", sessions.size());
+    }
+}
