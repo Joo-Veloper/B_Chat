2ad4e72f2a9ec22ea3e7e132e238fa7f9d19472b
Joo-Veloper <psjoo3515@gmail.com>
2025-02-28 17:47:18 +0900

    feat: Add TimeStamp to existing entities and create new ChatRoom, ChatMessage, ChatParticipant, ReadStatus entities

---
+++ src/main/java/io/chat/domain/chat/entity/ReadStatus.java
@@ -0,0 +1,35 @@
+package io.chat.domain.chat.entity;
+
+import io.chat.global.common.utils.TimeStamp;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReadStatus extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_message_id", nullable = false)
+    private ChatMessage chatMessage;
+
+    @Column(nullable = false)
+    private boolean isRead;
+
+}
--- src/main/java/io/chat/domain/member/entity/Member.java
+++ src/main/java/io/chat/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package io.chat.domain.member.entity;
 
+import io.chat.global.common.utils.TimeStamp;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -8,7 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class Member {
+public class Member extends TimeStamp {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
